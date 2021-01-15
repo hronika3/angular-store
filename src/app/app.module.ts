c086b7432204b8e94ba4c4d06a5e76186264ed65
hronika3 <sk_andr977@mail.ru>
2021-01-15 19:19:39 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import {SharedModule} from './admin/shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBfNmpwic98qlqHCEd2s7Tl3X_ZmGmzzyg'
+};
